a710f7494fabb39a46121f0c91317654e25296d8
PshrivastavaMoreYeahs <pshrivastava@taskhuman.com>
2026-06-12 16:49:00 +0530

    Initial Commit

---
+++ RemoteRecruit/RemoteRecruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRecruitApp.swift
+//  RemoteRecruit
+//
+//  Created by Prashant Shrivastava on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRecruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
